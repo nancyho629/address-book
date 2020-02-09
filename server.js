01e8faaa83831025b898805d36039c498cf66413
nancyho629 <nancyho629@gmail.com>
2020-02-09 15:41:04 -0500

    Add routes to user, contacts, and auth

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 app.get('/'), (req, res) => res.json({ "msg": "Welcome to the Contact Keeper API!"});
 
 // Define Routes
-
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on ${PORT}`))
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
