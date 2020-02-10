1e480b20e2029c01b7ef8333f9581289d391f425
nancyho629 <nancyho629@gmail.com>
2020-02-10 17:09:05 -0500

    Add gitignore and update GET route in auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,25 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-const config = require('config')
-const { check, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 // GET api/auth
-router.get('/', (req, res) => {
-  res.send('Get logged in user')
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user)
+  } catch(err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+  }
 } );
 
 // Post api/auth
+// @access Public
 router.post('/',
 [
   check('email', 'Please include a valid email').isEmail(),
