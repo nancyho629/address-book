5fbd48625cd866a2d14993a6f14e1c074de23a56
nancyho629 <nancyho629@gmail.com>
2020-02-19 13:47:47 -0500

    Add Register and Login components
    -Added Register and Login form components
    -Added "" to Navbar
    -Styling

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,6 +11,8 @@ const Navbar = ({ title, icon }) => {
         <ul>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/login'>Login</Link>
         </ul>
     </div>
   )
