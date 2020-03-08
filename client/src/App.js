5a55c6a54d3eb0ccff2346c32abb0a6398430911
nancyho629 <nancyho629@gmail.com>
2020-03-08 18:43:41 -0400

    Add PrivateRoute
    -Added private route functionality so only logged in users
    can navigate to '/' route

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import AlertState from './context/alert/AlertState'
 import Alerts from './components/layout/Alerts'
+import PrivateRoute from './components/routing/PrivateRoute'
 import setAuthToken from './utils/setAuthToken'
 
 if (localStorage) {
@@ -27,7 +28,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
